ed3f95435eef7ca771ac1b08102c2e55c36b2b04
Abhimanyu Kanjilal <abhimanyu.techno@gmail.com>
2023-11-22 17:10:19 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //MongoURI: "mongodb+srv://vivek:123@cluster0.9mrxijd.mongodb.net/?retryWrites=true&w=majority"
+MongoURI:"mongodb://souvik3469:souvik3469@ac-aln2hkj-shard-00-00.mqjshsn.mongodb.net:27017,ac-aln2hkj-shard-00-01.mqjshsn.mongodb.net:27017,ac-aln2hkj-shard-00-02.mqjshsn.mongodb.net:27017/?ssl=true&replicaSet=atlas-ohq1rx-shard-0&authSource=admin&retryWrites=true&w=majority"
+}
